74a24b94603f0da674011d5a861d6d6396b6e4af
Franco Cordoba <francocordobafs@gmail.com>
2025-10-17 01:47:15 -0300

    finished som ex

--- Clase_07/ejercicio01.cpp
+++ Clase_07/ejercicio01.cpp
@@ -4,25 +4,44 @@
 #include <iostream>
 using namespace std;
 
-void addPair(int arrayN[], int N)
+//agrega un valor par por cada indice del array
+void addPair(int arrayN[], int N) 
 {
     int num = 2;
     for (int i = 0; i < N; i++)
     {
-        if (num%2 == 0)
+        if (num % 2 == 0)
         {
             arrayN[i] = num;
+            num = num + 2;
         }
     }
-
     return;
-    
+}
+
+//imprime cada dato del array y los separa en ,
+void printArray(int arr[], int N)
+{
+    cout << "Array Data: ";
+    for (int i = 0; i < N; i++)
+    {
+        if (i != N - 1) // if para no ubicar la "," en el ultimo dato del array
+        {
+            cout << arr[i] << ", ";
+        }
+        else
+        {
+            cout << arr[i];
+        }
+    }
 }
 
 int main()
 {
-    int N = 7;
-    int par[N];
+    int N = 0;
+    cout << "Ingrese un Num. del 1 al 25" << endl;
+    cin >> N;
+    int par[N]; //declaro el array despues de CIN N. Una vez declarado no lo puedo modificar
     addPair(par, N);
-    cout << par;
+    printArray(par, N);
 }

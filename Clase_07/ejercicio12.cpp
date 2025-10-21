3968deae8222825ed66eb4c8afc2bad3a531680b
Franco Cordoba <francocordobafs@gmail.com>
2025-10-21 00:16:24 -0300

    finished 11

---
+++ Clase_07/ejercicio12.cpp
@@ -0,0 +1,8 @@
+/* 
+12. Ingresar un valor entero N (< 40). Luego ingresar un conjunto REFER de N elementos reales
+(ingresan ordenados por magnitud creciente). Finalmente ingresar un valor pesquisa X.
+Desarrollar el programa que determine e imprima:
+a) Con cual elemento (posición) del conjunto coincide, o
+b) Entre cuales dos elementos (posiciones) se encuentra, o
+c) Si es menor que el primero o mayor que el último.
+ */
